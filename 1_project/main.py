5775646ec23572bf3ec33aebb6ced6333cf41f9c
mmartinolli <mmartinolli@student.ethz.ch>
2025-10-27 17:01:04 +0100

    Fix

--- 1_project/main.py
+++ 1_project/main.py
@@ -28,15 +28,16 @@ def errase_equal_entries(X):
     return np.delete(X, vec, 1)
 
 # Use Mahalanobis distance to detect outliers. (X - mu)* Covariance_matrix^-1*(X - mu)^T
-# But still needs some work, as it does not filter anything yet :(
 def filter(X):
     _mean = np.mean(X, axis=0, keepdims=False)
     cov = np.cov(X, rowvar=False)
-    inv_cov = np.linalg.pinv(cov)
+    inv_cov = np.linalg.inv(cov)
     matrix = np.matmul(inv_cov, np.matrix.transpose(X - _mean))
     matrix = np.matmul((X - _mean), matrix)
     D2 = np.diag(matrix)
-    return X[D2 < (chi2.ppf(0.99, df=len(X[0])))]
+    print(len(X))
+    print(len(X[D2 < (chi2.ppf(0.995, df=len(X[0])))]))
+    return X[D2 < (chi2.ppf(0.995, df=len(X[0])))]
 
 
 _X_train = pd.read_csv("data/X_train.csv")
@@ -50,11 +51,9 @@ X_test = _X_test.values[:, 1:]
 X_test = impute(X_test)
 X_train = impute(X_train)
 
-X_test = errase_equal_entries(X_test)
 X_train = errase_equal_entries(X_train)
 
-X_test = filter(X_test)
 X_train = filter(X_train)
 
-X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
+# X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
 
