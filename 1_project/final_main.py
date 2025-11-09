98a78b6b3e6042edc6d6f7617d3e613172a81da9
mmartinolli <mmartinolli@student.ethz.ch>
2025-11-09 22:58:34 +0100

    bubu

--- 1_project/final_main.py
+++ 1_project/final_main.py
@@ -18,6 +18,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import r2_score
 from sklearn.gaussian_process import GaussianProcessClassifier
 from sklearn.gaussian_process.kernels import RBF
+from lightgbm import LGBMRegressor
 
 
 
@@ -106,74 +107,109 @@ X_train, X_test = erase_equal_entries(X_train, X_test)
 
 X_train, y_train = filter(X_train, y_train)
 
-X_train = SelectKBest(f_regression, k=700).fit_transform(X_train,y_train[:,0])
-X_train = SelectKBest(mutual_info_regression, k=650).fit_transform(X_train,y_train[:,0])
+# X_train = SelectKBest(f_regression, k=700).fit_transform(X_train,y_train[:,0])
+# X_train = SelectKBest(mutual_info_regression, k=650).fit_transform(X_train,y_train[:,0])
+selector1 = SelectKBest(f_regression, k=700)
+X_train = selector1.fit_transform(X_train, y_train[:, 0])
+X_test = selector1.transform(X_test)
 
-X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
-scaler = StandardScaler().fit(X_train)
-train_data = Mri_dataset(X_train, y_train)
-val_data = Mri_dataset(X_val, y_val)
-train_loader = DataLoader(train_data, batch_size=16, shuffle=True)
-val_loader = DataLoader(val_data, batch_size=16, shuffle=True)
+selector2 = SelectKBest(mutual_info_regression, k=650)
+X_train = selector2.fit_transform(X_train, y_train[:, 0])
+X_test = selector2.transform(X_test)
 
-num_features = len(X_train[0])
+# train_data = Mri_dataset(X_train, y_train)
+# val_data = Mri_dataset(X_val, y_val)
+# train_loader = DataLoader(train_data, batch_size=16, shuffle=True)
+# val_loader = DataLoader(val_data, batch_size=16, shuffle=True)
+
+# num_features = len(X_train[0])
 
 # Training process
 
-kernel = 1.0 * RBF(1.0)
+# kernel = 1.0 * RBF(1.0)
+model = LGBMRegressor(
+    n_estimators=1000,
+    learning_rate=0.01,
+    max_depth=-1,
+    subsample=0.8,
+    colsample_bytree=0.8
+)
+train_with_val = False #set to false if you want to submit a solution!!
+if train_with_val:
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
+    scaler = StandardScaler().fit(X_train)
 
-# X_train = scaler.transform(X_train)
-# X_val = scaler.transform(X_val)
+    X_train = scaler.transform(X_train)
+    X_val = scaler.transform(X_val)
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_val)
+    score = r2_score(y_val, y_pred)
+    print(f"R2 score for validation set is: {round(score, 2)}")
+else:
+    scaler = StandardScaler().fit(X_train)
+    X_train = scaler.transform(X_train)
+    model.fit(X_train, y_train)
+
+
+X_test = scaler.transform(X_test)
+pred = model.predict(X_test)
+ids = _X_test["id"]
+submission = pd.DataFrame({
+    "id": ids,
+    "target": pred
+})
+submission.to_csv("submission.csv", index=False)
+
 # gpc = GaussianProcessClassifier(kernel=kernel,random_state=0).fit(X_train, y_train)
 # y_pred = gpc.predict(X_val)
 # score = r2_score(y_val, y_pred)
 # print(r2_score(y_train, gpc.predict(X_train)))
 # print(f"R2 score for validation set is: {round(score, 2)}")
-model = Model(num_features).to(device)
-# model = nn.Linear(num_features, 1).to(device)
-criterion = nn.MSELoss()
-optimizer = optim.Adam(model.parameters(), lr=1e-3)
-epochs = 10
+# model = Model(num_features).to(device)
+# # model = nn.Linear(num_features, 1).to(device)
+# criterion = nn.MSELoss()
+# optimizer = optim.Adam(model.parameters(), lr=1e-3)
+# epochs = 10
 
-for k in range(epochs):
-    train_loss = 0
-    val_loss = 0
-    model.train()
-    for x_batch, y_batch in train_loader:
-        x_batch = x_batch.to(device)
-        y_batch = y_batch.to(device)
-        pred = model(x_batch)
-        loss = criterion(pred, y_batch)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        train_loss += loss.item()
-    print(f"Epoch {k + 1}: train_loss = {round(train_loss/len(X_train), 2)}")
-    model.eval()
-    with torch.no_grad():
-        for x_batch, y_batch in val_loader:
-            x_batch = x_batch.to(device)
-            y_batch = y_batch.to(device)
-            pred = model(x_batch)
-            val_loss += criterion(pred, y_batch).item()
-        print(f"Validation loss: {round(val_loss/len(X_val), 2)}")
+# for k in range(epochs):
+#     train_loss = 0
+#     val_loss = 0
+#     model.train()
+#     for x_batch, y_batch in train_loader:
+#         x_batch = x_batch.to(device)
+#         y_batch = y_batch.to(device)
+#         pred = model(x_batch)
+#         loss = criterion(pred, y_batch)
+#         optimizer.zero_grad()
+#         loss.backward()
+#         optimizer.step()
+#         train_loss += loss.item()
+#     print(f"Epoch {k + 1}: train_loss = {round(train_loss/len(X_train), 2)}")
+#     model.eval()
+#     with torch.no_grad():
+#         for x_batch, y_batch in val_loader:
+#             x_batch = x_batch.to(device)
+#             y_batch = y_batch.to(device)
+#             pred = model(x_batch)
+#             val_loss += criterion(pred, y_batch).item()
+#         print(f"Validation loss: {round(val_loss/len(X_val), 2)}")
     
 
 
 
 
 #Calculate score
-model.eval()
-with torch.no_grad():
-    preds = []
-    trues = []
-    for x_batch, y_batch in val_loader:
-        x_batch = x_batch.to(device)
-        preds.append(model(x_batch).squeeze(-1).cpu())
-        trues.append(y_batch.squeeze(-1).cpu())
+# model.eval()
+# with torch.no_grad():
+#     preds = []
+#     trues = []
+#     for x_batch, y_batch in val_loader:
+#         x_batch = x_batch.to(device)
+#         preds.append(model(x_batch).squeeze(-1).cpu())
+#         trues.append(y_batch.squeeze(-1).cpu())
 
-y_pred = torch.cat(preds).numpy()
-y_true = torch.cat(trues).numpy()
-score = r2_score(y_true, y_pred)
-print(f"R2 score for validation set is: {round(score, 2)}")
+# y_pred = torch.cat(preds).numpy()
+# y_true = torch.cat(trues).numpy()
+# score = r2_score(y_true, y_pred)
+# print(f"R2 score for validation set is: {round(score, 2)}")
 
